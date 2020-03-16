e359056ea7570b4f5464ab6b7ed9cf15f4cccc7f
Alexey Shabginov <a.shabginov@gmail.com>
2020-03-16 13:34:22 -0700

    Added mouseover event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+// Mouseover
+let links = document.querySelectorAll('.nav-link');
+function removeLink(event) {
+  event.target.style.display = 'none';
+}
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('mouseover', removeLink);
+}
